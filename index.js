6542ed06d2e2f146dd5449f0ffdf1f91367e98a4
Fer Hernandez <fh.1111@gmail.com>
2019-02-06 03:08:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,19 +11,19 @@ function moveDodgerLeft() {
   }
 }
 
-function moveDodgerRight() {
-  var rightNumbers = dodger.style.right.replace('px', '')
-  var right = parseInt(rightNumbers, 10)
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
 
-
-    dodger.style.right = `${right + 1}px`
-
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`
+  }
 }
 
 document.addEventListener('keydown', function(e) {
-  //if (e.which === 37) {
-  //  moveDodgerLeft()
-  //} 
+  if (e.which === 37) {
+    moveDodgerLeft()
+  }
   if (e.which === 39) {
     moveDodgerRight()
   }
